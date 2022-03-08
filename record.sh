17ce51ecb6e97c54f9d387e59211a3f05cc4dd59
Samuel Lord <sam@sllord.info>
2022-03-08 02:11:01 -0600

    Update record.sh

--- record.sh
+++ record.sh
@@ -3,6 +3,7 @@ resscreen=$(xdpyinfo | awk '/dimensions:/ { print $2 }')
 audioout="alsa_output.usb-Logitech_USB_Headset_Logitech_USB_Headset-00.analog-stereo.monitor"
 audioin="alsa_input.usb-Logitech_USB_Headset_Logitech_USB_Headset-00.mono-fallback"
 
+echo We are starting the screencast!
 ffmpeg -f pulse -i $audioin -ac 1 -f mp3 - | ffplay - |  ffmpeg -f x11grab -thread_queue_size 450  -framerate 45 -video_size $resscreen -i :0.0 \
        -f v4l2 -thread_queue_size 64 -video_size 320x180 -framerate 30 -thread_queue_size 225 -i /dev/video0 \
        -f pulse -i $audioout -ac 2 \
